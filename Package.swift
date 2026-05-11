a1f14138dd39076f415e6ecf1ef2861d2529d94a
Quang Huynh <quanghuynh@gmail.com>
2026-05-11 09:28:51 -0400

    Release 0.1.29-beta.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
        .binaryTarget(
             name: "UnifoldSDK",
-            url: "https://github.com/unifold-io/unifold-ios/releases/download/0.1.28/UnifoldSDK.xcframework.zip",
-            checksum: "3376deb0c61273be4a2d72fd5de3ede12910c47ebf4defa996d61a8e1a42f072"
+            url: "https://github.com/unifold-io/unifold-ios/releases/download/0.1.29-beta.1/UnifoldSDK.xcframework.zip",
+            checksum: "cceff9afaa83e0ca5ac85cf6f707a442b2c245f0e92959c43921e576f9554755"
         )
     ]
 )
